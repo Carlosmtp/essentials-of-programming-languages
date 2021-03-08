145280a2ee181656ac326e842b0f1003302afd8f
Carlos Mauricio Tovar Parra <42880933+Carlosmtp@users.noreply.github.com>
2021-03-08 15:38:15 -0500

    Create taller2.rkt

---
+++ taller2.rkt
@@ -0,0 +1,6 @@
+#lang eopl
+;Taller 2 - Fundamentos de Lenguajes de Programación
+
+;Diana Katherine Toro Ortiz - 2110046
+;Carlos Mauricio Tovar Parra - 1741699
+;Juan Pablo Velasco Mellizo - 1766616
