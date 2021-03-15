82a303a89271802ccbb2b1c58647a215c7d77512
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2021-03-15 00:45:34 -0500

    añadidas las definiciones para la gramática de ambiente

--- taller2/extend-env.rkt
+++ taller2/extend-env.rkt
@@ -4,3 +4,39 @@
 ;Diana Katherine Toro Ortiz - 2110046
 ;Carlos Mauricio Tovar Parra - 1741699
 ;Juan Pablo Velasco Mellizo - 1766616
+
+;Env = (empty-env) | (extend-env Var SchemeVal Env)
+;Var = Sym
+
+;empty-env : () → Env
+(define empty-env
+  (lambda () (list 'empty-env)))
+
+;extend-env : Var × SchemeVal × Env → Env
+(define extend-env
+  (lambda (var val env)
+    (list 'extend-env var val env)))
+
+;apply-env : Env × Var → SchemeVal
+(define apply-env
+  (lambda (env search-var)
+    (cond
+      ((eqv? (car env) 'empty-env)
+       (report-no-binding-found search-var))
+((eqv? (car env) 'extend-env)
+(let ((saved-var (cadr env))
+(saved-val (caddr env))
+(saved-env (cadddr env)))
+(if (eqv? search-var saved-var)
+saved-val
+(apply-env saved-env search-var))))
+(else
+(report-invalid-env env)))))
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+
+(define report-invalid-env
+  (lambda (env)
+    (eopl:error apply-env "Bad environment: ~s" env)))
