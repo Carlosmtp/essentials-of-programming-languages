4b1894b1d1b22774d26188144f6d13a0b1b11850
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2021-03-22 09:08:36 -0500

    corregida la definición de bignum, para que funcione con el código fuente dado

--- taller2/bignum32.rkt
+++ taller2/bignum32.rkt
@@ -10,21 +10,21 @@
 ;Implementación Bignum para N=32
 
 (define N 32)
-(define zero '())
+(define zero (lambda () '()))
 (define is-zero? (lambda (n) (or (null? n) (equal? n '(0)))))
 (define valid-in-N-base?
   (lambda (n)
     (cond
-      [(null? n) #t]
+      [(is-zero? n) #t]
       [(>= (car n) N) #f]
       [(<(car n) N) (valid-in-N-base? (cdr n))])))
 
 (define successor
   (lambda (n)
     (cond
+      [(is-zero? n) (list 1)]
       [(valid-in-N-base? n)
        (cond
-         [(is-zero? n) '(1)]
          [(= (car n) (- N 1))
           (cons 0 (successor (cdr n)))]
          [(< (car n) (- N 1)) (cons (+ (car n) 1 ) (cdr n))])]
@@ -33,10 +33,10 @@
 (define predecessor
     (lambda (n)
     (cond
+      [(is-zero? n) (eopl:error 'Bignum "No se pueden representar números menores que 0")]
       [(valid-in-N-base? n)
        (cond
-         [(is-zero? n) (eopl:error 'Bignum "No se pueden representar números menores que 0")]
-         [(equal? n (successor zero)) '()]
+         [(equal? n (successor '())) '()]
          [(= (car n) 0) (cons (- N 1) (predecessor (cdr n)))]
          [else (cons (- (car n) 1 ) (cdr n))])]
       [else (eopl:error 'Bignum "No se pueden representar números con dígitos mayores o iguales a N")])))
@@ -61,7 +61,7 @@
 (define multiplicacion
   (lambda (x y)
     (if (is-zero? x)
-        zero
+        (zero)
         (suma (multiplicacion (predecessor x) y) y))
     ))
     
@@ -86,20 +86,20 @@
 
 (check-equal? (resta '(5 2) '(3 1)) '(2 1)) ; En decimal: 69 - 35 = 34
 (check-equal? (resta '(24 31) '(15 8)) '(9 23)) ; En decimal: 1016 - 271 = 745
-(check-equal? (resta '() '(0)) zero) ; En decimal: 0 - 0 = 0
+(check-equal? (resta '() '(0)) '()) ; En decimal: 0 - 0 = 0
 (check-equal? (resta '(13 25) '(0)) '(13 25)) ; 813 - 0 = 813
 
 (check-equal? (multiplicacion '(5 3) '(8 1)) '(8 30 3)) ; En decimal: 101 * 40 = 4040
 (check-equal? (multiplicacion '(2 4) '(6 2)) '(12 28 8)) ; En decimal: 130 * 70 = 9100
-(check-equal? (multiplicacion '(31 4) '()) zero) ; En decimal: 160 * 0 = 0
-(check-equal? (multiplicacion '(0) '(2 29)) zero) ; En decimal: 0 * 66 = 0
+(check-equal? (multiplicacion '(31 4) '()) '()) ; En decimal: 160 * 0 = 0
+(check-equal? (multiplicacion '(0) '(2 29)) '()) ; En decimal: 0 * 66 = 0
 
 (check-equal? (potencia '(2 4) '(2)) '(4 16 16)) ; En decimal: 130 ^ 2 = 16900
 (check-equal? (potencia '(15) '(3)) '(15 9 3)) ; En decimal: 15 ^ 3 = 3375
 (check-equal? (potencia '(1 7) '(0)) '(1)) ; En decimal: 71 ^ 0 = 1
-(check-equal? (potencia '(0) '(2)) zero) ; En decimal: 0 ^ 2 = 0
+(check-equal? (potencia '(0) '(2)) '()) ; En decimal: 0 ^ 2 = 0
 
 (check-equal? (factorial '(5)) '(24 3)) ; En decimal: 5! = 120
-(check-equal? (factorial zero) '(1)) ; En decimal: 0! = 1
+(check-equal? (factorial '()) '(1)) ; En decimal: 0! = 1
 (check-equal? (factorial '()) (factorial '(1))) ; En decimal: 0! = 1!
 (check-equal? (factorial '(8)) '(0 12 7 1)) ; En decimal: 8! = 40320
