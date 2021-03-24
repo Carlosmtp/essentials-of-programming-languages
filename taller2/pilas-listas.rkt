5cecde5afa81031326ed68fa15c0669d3878737c
Diana Toro <toro.diana@correounivalle.edu.co>
2021-03-24 16:34:07 -0500

    pilas con listas y datatype comentados

--- taller2/pilas-listas.rkt
+++ taller2/pilas-listas.rkt
@@ -4,38 +4,43 @@
 ;Carlos Mauricio Tovar Parra - 1741699
 ;Juan Pablo Velasco Mellizo - 1766616
 
+;       Gramática
+
 ; <stack> ::= (empty-stack)
 ;              ::=(push <scheme-value> <stack>)
 
-;empty stack
+
+;----------------------------------------------------------------------------------------------------
+
+;empty-stack : () -> list(empty-stack)
+;usage : (empty-stack()) definimos la lista vacía
+
 (define empty-stack
   (lambda ()
     (list 'empty-stack)))
 
-;empty stack?
+;----------------------------------------------------------------------------------------------------
+
+;empty-stack? : stack -> boolean
+;usage: (empty-stack? stack) retorna un booleano al comparar si el stack que recibe es vacío
+
 (define empty-stack?
   (lambda (stack)
     (equal? (car stack) 'empty-stack)))
 
-;push stack
+;pruebas
+(empty-stack? '(empty-stack))
+
+;----------------------------------------------------------------------------------------------------
+
+;push : stack -> list
+;usage : (push val stack) retorna una lista con el valor insertado al inicio del stack
+
 (define push
   (lambda(val stack)
     (list 'push val stack)))
 
-;pop
-(define pop
-  (lambda (stack)
-    (cond
-      [(empty-stack? stack)'()]
-      [else (caddr stack)])))
-
-;top
-(define top
-  (lambda (stack)
-    (cond
-      [(empty-stack? stack)'()]
-      [else (cadr stack)])))
-
+;prueba
 (define s1
   (push 'a
         (push 'b
@@ -44,7 +49,32 @@
                           (empty-stack))))))
 
 (push "s" '(empty-stack))
-(empty-stack? '(empty-stack))
 (push "s" s1)
-(pop f1)
+
+;----------------------------------------------------------------------------------------------------
+
+;pop : stack -> list
+;usage : (pop stack) retorna una lista retirando el elemento inicial del stack
+
+(define pop
+  (lambda (stack)
+    (cond
+      [(empty-stack? stack)'()]
+      [else (caddr stack)])))
+
+;prueba
+(pop s1)
+
+;----------------------------------------------------------------------------------------------------
+
+;top : stack -> list
+;usage : (top stack) retorna el elemento superior del stack
+
+(define top
+  (lambda (stack)
+    (cond
+      [(empty-stack? stack)'()]
+      [else (cadr stack)])))
+
+;prueba
 (top(pop s1))
